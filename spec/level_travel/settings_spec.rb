14acd86dc0fe77255080984ed60eb8907d2fa205
Pavel Rodionov <pasha.rod@mail.ru>
2020-03-04 22:15:34 +0300

    add settings for errors

--- spec/level_travel/settings_spec.rb
+++ spec/level_travel/settings_spec.rb
@@ -13,6 +13,27 @@ RSpec.describe LevelTravel::Settings do
     end
   end
 
+  describe '.error_load_path' do
+    subject(:set_path) { described_class.error_load_path = expected_path }
+
+    let(:expected_path) { '/path/to/errors.yml' }
+
+    after { described_class.error_load_path = nil }
+
+    it 'returns set value' do
+      expect { set_path }.
+        to change(described_class, :error_load_path).from(described_class::DEFAULT_ERROR_LOAD_PATH).to(expected_path)
+    end
+
+    context 'when it is not set' do
+      subject(:error_load_path) { described_class.error_load_path }
+
+      it 'returns default timeout' do
+        expect(error_load_path).to eq(described_class::DEFAULT_ERROR_LOAD_PATH)
+      end
+    end
+  end
+
   describe '.timeout' do
     subject(:timeout) { described_class.timeout }
 
